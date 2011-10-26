669a259a0ca9bd6da07695892015187d5434762d
Markus Strauss <Markus@ITstrauss.eu>
2011-10-26 15:59:03 -0400

    we need a mailer for the unattended-upgrade mails

--- manifests/classes/unattended-upgrade.pp
+++ manifests/classes/unattended-upgrade.pp
@@ -2,4 +2,8 @@ class apt::unattended-upgrade {
   package {"unattended-upgrades":
     ensure => present,
   }
+  
+  # for the mail-sending feature
+  package { 'heirloom-mailx': ensure => present }
+
 }
